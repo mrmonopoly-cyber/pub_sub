10a876871063031cecfc3f31a35177fa6262d6ea
Alberto Damo <alberto.damo@gmail.com>
2024-03-27 18:38:44 +0100

    refactoring broker methods name

--- message_borker.h
+++ message_borker.h
@@ -6,13 +6,14 @@
 typedef struct broker broker;
 
 //server
-broker* init_broker(const address* addr, const unsigned int port);
-void start_listening(broker* br);
-void close_listening(broker* br);
-void free_broker(broker* br);
+broker* broker_init(const address* addr, const unsigned int port);
+void broker_start_listening(broker* br);
+void broker_close_listening(broker* br);
+address* broker_get_addr(const broker *br);
+void broker_free(broker* br);
 //client
-void subscribe_to_channel_broker(const address* br_addr, const char** chann_subs, 
+void broker_subscribe_to_channel_broker(const address* br_addr, const char** chann_subs, 
         const unsigned int ch_num);
-void publish_broker(const address* br_addr, const char** chann_subs, const unsigned int ch_num, 
+void broker_publish_broker(const address* br_addr, const char** chann_subs, const unsigned int ch_num, 
         const void* data,const unsigned int data_size);
 
